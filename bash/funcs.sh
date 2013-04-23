59c3c6962cad50fe4a8dad33942654de15e779ba
Jon Binney <jbinney@willowgarage.com>
2013-04-23 12:56:04 -0700

    change terminal title when using ssh_

--- bash/funcs.sh
+++ bash/funcs.sh
@@ -1,8 +1,19 @@
 function ssh_()
 {
-     /usr/bin/dbus-send --session /net/sf/roxterm/Options net.sf.roxterm.Options.SetColourScheme string:$ROXTERM_ID string:Remote
-     /usr/bin/ssh "$@"
-     /usr/bin/dbus-send --session /net/sf/roxterm/Options net.sf.roxterm.Options.SetColourScheme string:$ROXTERM_ID string:Tango
+    # change window color
+    /usr/bin/dbus-send --session /net/sf/roxterm/Options net.sf.roxterm.Options.SetColourScheme string:$ROXTERM_ID string:Remote
+    
+    # set terminal title
+    echo -ne "\033]0;$@\007"
+    
+    # do the actually ssh
+    /usr/bin/ssh "$@"
+    
+    # set terminal title back to local
+    echo -ne "\033]0;local\007"
+    
+    # revert the termnial color
+    /usr/bin/dbus-send --session /net/sf/roxterm/Options net.sf.roxterm.Options.SetColourScheme string:$ROXTERM_ID string:Tango
 }
 
 function wgvpn()

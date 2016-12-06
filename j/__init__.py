072eb7b6df9f2e8330d44fea92575730905cfcae
Jon Binney <jon.binney@gmail.com>
2016-12-06 15:19:35 -0800

    Remove obsolete files

--- j/__init__.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-# within j
-import ssh, install, dropbox, ros
-
-def run(cmd):
-    output, return_code = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()
-    return output

b15f45b619bb6e98b38091c1ded01c81abf224a7
Jon Binney <jon.binney@gmail.com>
2018-11-08 11:33:16 -0800

    Bind reverse search in zsh vi mode

--- sh/rc.zsh
+++ sh/rc.zsh
@@ -3,4 +3,7 @@ export SH_LOCAL_DIR=$HOME/config/sh
 # Use vi keybindings
 bindkey -v
 
+# vi mode doesn't bind reverse seach by default
+bindkey "^R" history-incremental-search-backward
+
 source ${SH_LOCAL_DIR}/rc.sh
